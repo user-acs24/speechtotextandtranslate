5d74cdde2901053a5f6b4b083fa9912847568961
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-08-31 20:32:27 +0530

    Basic structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import styles from './App.css'
+// import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className={styles.container}>
+        <h2>Speech to Text Converter</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
