f3c1e5ea1e22163c8fb34cdf09e795e06c294582
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-09-01 15:45:17 +0530

    Styling layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import styles from './App.css'
+import styles from './app.module.css'
 // import React from 'react'
 
 function App() {
@@ -8,6 +8,12 @@ function App() {
     <>
       <div className={styles.container}>
         <h2>Speech to Text Converter</h2>
+        <div className={styles.mainContent}></div>
+        <div className={styles.btn}>
+          <button>Copy</button>
+          <button>Start</button>
+          <button>Stop</button>
+        </div>
       </div>
     </>
   )
