777666aa67b38317a3834757e91af8c12204d4f3
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-09-02 14:01:05 +0530

    Functionalities implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 // import { useState } from 'react'
 import styles from './app.module.css'
-// import React from 'react'
-import { BiSolidCopyAlt } from "react-icons/bi";
-import { BsFillMicFill } from "react-icons/bs";
-import { BsFillMicMuteFill } from "react-icons/bs";
+import { useState } from 'react'
+import 'regenerator-runtime/runtime'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from "react-use-clipboard"
+import { BiSolidCopyAlt } from "react-icons/bi"
+import { BsFillMicFill } from "react-icons/bs"
+import { BsFillMicMuteFill } from "react-icons/bs"
+import { SiConvertio } from "react-icons/si"
 
-function App() {
+const App = () => {
+
+  const [copyTxt, setCopyTxt] = useState()
+  const [isCopied, setCopied] = useClipboard(copyTxt);
+
+  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-In' })
+  const stopListening = () => SpeechRecognition.stopListening()
+
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+
+  if (!browserSupportsSpeechRecognition) {
+    return alert('no browsersupport')
+  }
 
   return (
     <>
       <div className={styles.container}>
-        <h1>Speech to Text Converter</h1>
-        <div className={styles.mainContent}></div>
+        <h1>Speech to Text Converter <SiConvertio className={styles.headIcon} /></h1>
+        <p>Note: To copy written text, firstly click once on the white board after clicking Stop button.</p>
+        <div className={styles.mainContent} onClick={() => setCopyTxt(transcript)}>
+          {transcript}
+        </div>
         <div className={styles.btn}>
-          <button><BiSolidCopyAlt />Copy</button>
-          <button><BsFillMicFill />Start</button>
-          <button><BsFillMicMuteFill />Stop</button>
+          <button onClick={startListening}><BsFillMicFill />Start</button>
+          <button onClick={stopListening}><BsFillMicMuteFill />Stop</button>
+          <button onClick={setCopied}>
+            <BiSolidCopyAlt />{isCopied ? " Copied" : " Copy to clipboard"}
+          </button>
         </div>
       </div>
     </>
